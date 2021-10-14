5506ef84be44048d97ca6ad1e64b0cdc08f3a338
Ярослав Дрововозов <byster1234@mail.ru>
2021-10-14 10:28:00 +0300

    Добавьте файлы проекта.

---
+++ Lab1/View/WindowEmailPerson.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Lab1.ViewModel;
+namespace Lab1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowEmailPerson.xaml
+    /// </summary>
+    public partial class WindowEmailPerson : Window
+    {
+        public WindowEmailPerson()
+        {
+            InitializeComponent();
+            EmailPersonVievModel vmEmailPerson = new EmailPersonVievModel();
+            lvEmailPerson.ItemsSource = vmEmailPerson.ListEmailPerson;
+        }
+    }
+}
---
+++ Lab1/ViewModel/EmailPersonVievModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Lab1.Model;
+
+namespace Lab1.ViewModel
+{
+    class EmailPersonVievModel
+    {
+        public ObservableCollection<EmailPerson> ListEmailPerson { get; set; } = new
+       ObservableCollection<EmailPerson>();
+        public EmailPersonVievModel()
+        {
+            this.ListEmailPerson.Add(new EmailPerson
+            {
+                Id = 1,
+                PersonID = 1,
+                Email = "byster1234@mail.ru"
+            });
+            this.ListEmailPerson.Add(new EmailPerson
+            {
+                Id = 2,
+                PersonID = 2,
+                Email = "alexdoljenko@mail.ru"
+            });
+            this.ListEmailPerson.Add(new EmailPerson
+            {
+                Id = 3,
+                PersonID = 3,
+                Email = "luxury1932@yandex.ru"
+            });
+        }
+    }
+}
